d10a505865816a1fa388084e57e351fb576e9ad7
alexv <asmodehn@gmail.com>
2017-09-01 18:50:36 +0900

    registering hypothesis profile earlier, at import time.

--- tests/test_pyros_schemas/conftest.py
+++ tests/test_pyros_schemas/conftest.py
@@ -5,19 +5,18 @@ import pytest
 import hypothesis
 
 
-def pytest_runtest_setup(item):
-    if hasattr(hypothesis, 'HealthCheck'):
-        hypothesis.settings.register_profile("travis", hypothesis.settings(
-            suppress_health_check=hypothesis.HealthCheck.too_slow
-        ))
-    else:
-        hypothesis.settings.register_profile("travis", hypothesis.settings(
-            # default
-        ))
-
-    hypothesis.settings.register_profile("dev", hypothesis.settings(
-        verbosity=hypothesis.Verbosity.verbose,
+if hasattr(hypothesis, 'HealthCheck'):
+    hypothesis.settings.register_profile("travis", hypothesis.settings(
+        suppress_health_check=getattr(hypothesis.HealthCheck, 'too_slow')
+    ))
+else:
+    hypothesis.settings.register_profile("travis", hypothesis.settings(
+        # default
     ))
 
-    # default settings
-    hypothesis.settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'dev'))
+hypothesis.settings.register_profile("dev", hypothesis.settings(
+    verbosity=hypothesis.Verbosity.verbose,
+))
+
+# default settings
+hypothesis.settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'dev'))
